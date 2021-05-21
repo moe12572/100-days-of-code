24f9ebe50074c948b509192ed5d7e228360d5afc
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-05-21 12:46:16 -0400

    Succesffuly completed isValid Leetcode Prob

---
+++ Days/Day6/codingChallenge.js
@@ -0,0 +1,39 @@
+/*
+var isValid = function(s) {
+    var splitStr = s.split('');
+    var stack = [];
+    if(s.length%2 !== 0){
+        return false;
+    }
+    for(var i = 0; i < splitStr.length; i++){
+        if(splitStr[i] === '{' || splitStr[i] === '[' || splitStr[i] === '('){
+            stack.push(splitStr[i]);
+            continue;
+        }
+        if(splitStr[i] === '}' && stack[stack.length-1] === '{'){
+            stack.pop()
+            continue;
+        }
+        if(splitStr[i] === ']' && stack[stack.length-1] === '['){
+            stack.pop()
+            continue;
+        }
+        if(splitStr[i] === ')' && stack[stack.length-1] === '('){
+            stack.pop()
+            continue;
+        } else {
+            return false;
+        }
+    }
+    if(stack.length !== 0){
+        return false;
+    }
+    return true;
+};
+
+s = '{}[]{}'
+console.log(isValid(s))
+
+//string of char
+//boolean
+*/
